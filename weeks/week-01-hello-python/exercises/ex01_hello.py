5e44491fb2e79bb13800224274bd992ab0b4fc7d
NguyenThuyLinh <thuylinh582007@gmail.com>
2026-09-18 20:47:08 +0700

    ;rgb:bfbf/bfbf/bfbfrgb:0000/0000/0000wr

--- weeks/week-01-hello-python/exercises/ex01_hello.py
+++ weeks/week-01-hello-python/exercises/ex01_hello.py
@@ -9,11 +9,11 @@ Chạy file này bằng: python ex01_hello.py
 
 # TODO 1: In ra dòng chữ "Hello, World!"
 # Viết code ở đây:
-
+print("Hello, World!")
 
 # TODO 2: In ra tên của bạn trên một dòng riêng
 # Viết code ở đây:
-
+print("Nguyễn Thùy Linh")
 
 # TODO 3: In ra 3 điều bạn thích, mỗi điều trên một dòng
 # Ví dụ:
@@ -21,7 +21,9 @@ Chạy file này bằng: python ex01_hello.py
 # Tôi thích nghe nhạc
 # Tôi thích code Python
 # Viết code ở đây:
-
+print("Tôi thích ăn đồ nướng")
+print("Tôi thích chơi game")
+print("Tôi thích HAK")
 
 # TODO 4: In ra một hình chữ nhật bằng dấu *
 # Kết quả mong muốn:
@@ -30,6 +32,10 @@ Chạy file này bằng: python ex01_hello.py
 # *   *
 # *****
 # Viết code ở đây:
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
 
 
 # TODO 5 (Thử thách): In ra tên bạn bằng ký tự đặc biệt
@@ -40,6 +46,9 @@ Chạy file này bằng: python ex01_hello.py
 # H   H   I
 # H   H  III
 # Viết code ở đây:
-
-
+print("L         IIIII  NN      N   H     H")        
+print("L           I    N  N    N   H     H")              
+print("L           I    N    N  N   HHHHHHH")     
+print("L           I    N      NN   H     H")     
+print("LLLLLLLL  IIIII  N       N   H     H")          
 print("\n🎉 Chúc mừng! Bạn đã hoàn thành bài tập đầu tiên!")
--- weeks/week-01-hello-python/exercises/ex03_input.py
+++ weeks/week-01-hello-python/exercises/ex03_input.py
@@ -6,19 +6,31 @@ Mục tiêu: Sử dụng input() để nhận dữ liệu từ người dùng
 
 # TODO 1: Hỏi tên người dùng và in lời chào
 # Ví dụ: "Xin chào, Minh!"
-
+ten  = input("Bạn tên là gì? ")
+print("xin chào,",ten)
 
 # TODO 2: Hỏi tuổi người dùng, tính và in năm sinh
 # Gợi ý: Nhớ chuyển input sang int!
-
+tuoi = int(input("Bạn bao nhiêu tuổi? "))
+namsinh = 2026 - tuoi
+print("BẠn sinh năm:",namsinh)
 
 # TODO 3: Hỏi người dùng nhập 2 số, tính và in tổng
-# Ví dụ output:
+# Ví dụ output:li
 # Nhập số thứ nhất: 15
 # Nhập số thứ hai: 27
 # Tổng: 15 + 27 = 42
-
-
+so1 = int(input("Nhập số thứ nhất: "))
+so2 = int(input("Nhập số thứ hai: "))
+tong = so1 + so2
+print(f"Tổng hai số là: {so1} + {so2} = {tong}")
 # TODO 4 (Thử thách): Tạo Mad Libs mini
 # Hỏi người dùng nhập: tên, tính từ, con vật, số
 # Rồi in ra câu chuyện vui
+tenn = input("Hãy nhập 1 cái tên:")
+tinhtu = input("Hãy chọn tính từ (ngố, dễ thương, béo): ")
+convat = input("Hãy chọn một con vật (chó, mèo, cú): ")
+so = input("Hãy chọn 1 con số: ")
+print("\n---CÂU CHUYỆN VUI CỦA BẠN---")
+print(f"{tenn} là con {convat} rất {tinhtu}.")
+print(f"Mỗi ngày nó ăn hết {so} cám heo")
